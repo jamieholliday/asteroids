1d7171d95e0913410653995fa7ad3b1299232343
jamieholliday <jamie@jamieholliday.com>
2025-01-10 19:13:41 +0000

    feat: black screen

---
+++ main.py
@@ -0,0 +1,31 @@
+import pygame
+
+from constants import (
+    ASTEROID_KINDS,
+    ASTEROID_MAX_RADIUS,
+    ASTEROID_MIN_RADIUS,
+    ASTEROID_SPAWN_RATE,
+    SCREEN_HEIGHT,
+    SCREEN_WIDTH,
+)
+
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
