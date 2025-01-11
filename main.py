50ab9acdbce77c05607f23479c47a960fcd04a9d
jamieholliday <jamie@jamieholliday.com>
2025-01-11 12:37:43 +0000

    feat: add shapes to screen

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import (
     ASTEROID_KINDS,
     ASTEROID_MAX_RADIUS,
@@ -8,6 +10,7 @@ from constants import (
     SCREEN_HEIGHT,
     SCREEN_WIDTH,
 )
+from player import Player
 
 
 def main():
@@ -16,6 +19,16 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = updatable
+    Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    AsteroidField()
 
     while True:
         for event in pygame.event.get():
@@ -24,7 +37,14 @@ def main():
                 return
 
         screen.fill((0, 0, 0))
+
+        for updateable_sprite in updatable:
+            updateable_sprite.update(dt)
+        for drawable_sprite in drawable:
+            drawable_sprite.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000.0
 
 
 if __name__ == "__main__":
